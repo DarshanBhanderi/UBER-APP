f0d632f60e83d32d01a05b16a77d550a5b9183e1
Darshan Bhanderi <darshanbhanderi2929@gmail.com>
2025-10-09 21:41:15 +0530

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 // import { useGSAP } from '@gsap/react';
 // import gsap from 'gsap';
- //////import axios from 'axios';
+////import axios from 'axios';
 // import 'remixicon/fonts/remixicon.css'
 // import LocationSearchPanel from '../components/LocationSearchPanel';
 // import VehiclePanel from '../components/VehiclePanel';
