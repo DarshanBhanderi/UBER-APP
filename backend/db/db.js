4d8589b6b0e2e47566e02bfd4a88fccd5eabb861
Darshan Bhanderi <darshanbhanderi2929@gmail.com>
2026-04-07 00:30:33 +0530

    fixed mongodb connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,24 @@
+// const mongoose = require('mongoose');
+
+
+// function connectToDb() {
+//     mongoose.connect(process.env.MONGO_PATH
+//     ).then(() => {
+//         console.log('Connected to DB');
+//     }).catch(err => console.log(err));
+// }
+
+
+// module.exports = connectToDb;
+
 const mongoose = require('mongoose');
 
-
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT
-    ).then(() => {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
         console.log('Connected to DB');
-    }).catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 }
-
 
 module.exports = connectToDb;
